bb4a9e6f3aa15bd689edf6188e276c76f78728f2
alessiapicillo <alessia.picillo@stud.unifi.it>
2023-07-06 13:03:20 +0200

    Classe registro delle chat

---
+++ ChatRegister.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alessia Picillo on 06/07/23.
+//
+
+#include "ChatRegister.h"
---
+++ ChatRegister.h
@@ -0,0 +1,32 @@
+//
+// Created by Alessia Picillo on 06/07/23.
+//
+
+#ifndef LABPROG_CHATREGISTER_H
+#define LABPROG_CHATREGISTER_H
+#include "User.h"
+#include "Message.h"
+#include "Chat.h"
+#include <iostream>
+
+class ChatRegister {
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //LABPROG_CHATREGISTER_H
