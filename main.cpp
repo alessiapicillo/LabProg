a8f01869e16eccd9a0f467f7d09a9f4cc9a02cd0
alessiapicillo <alessia.picillo@stud.unifi.it>
2023-07-06 14:23:45 +0200

    Due utenti casuali

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "User.h"
+#include "Message.h"
+#include "Chat.h"
+#include "ChatRegister.h"
 
 //Classi che rappresentano chat tra utenti. Si deve rappresentare un utente di un sistema,
 //un messaggio di chat da un utente ad un altro, una classe che rappresenta una chat tra due utenti,
 //un registro di tutte le chat fatte.
 
 int main() {
-
+User user1("Alessia");
+User user2("Marco");
 }
